dd2da6f6ebf81ed21ed257bac84ef6a5111fcd5c
Mark01xx <126393298+Mark01xx@users.noreply.github.com>
2024-09-25 23:33:38 +0800

    Add files via upload

---
+++ dbconnect .php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "signup";
+$conn = "";
+
+
+
+try{
+
+
+    $conn = mysqli_connect ($db_server, $db_user, $db_pass, $db_name);
+}
+
+    catch(mysqli_sql_exeption){
+        echo "could not connect";
+    }
+
+?>
